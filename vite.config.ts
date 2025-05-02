c522a1fb933eb137a8d8e23eeadbac79e66d43f5
Derek Au <derek@derekau.net>
2025-05-01 23:22:32 -0400

    vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vite-plaster-calculator/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
